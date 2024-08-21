47cf987b96659d349c391ebb04888c581a505f24
martupiru <martunahman@gmail.com>
2024-08-21 18:40:40 -0300

    Cambios code

--- tp2-agentes-racionales/code/agent.py
+++ tp2-agentes-racionales/code/agent.py
@@ -8,11 +8,5 @@ class Agent:
         # Recibe la accion y la ejecuta
         return action
     
-def obtener_accion_usuario():
-    while True:
-        action = input('Introduce la acción: Arriba, Abajo, Izquierda, Derecha, Limpiar, NoHacerNada: ')
-        if action in ['Arriba', 'Abajo', 'Izquierda', 'Derecha', 'Limpiar', 'NoHacerNada']:
-            return action
-        else:
-            print("Acción no válida. Por favor, elige una acción de la lista.")
+
     
